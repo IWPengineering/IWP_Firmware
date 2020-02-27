aeaa66ea8b40d44729fee2f23171d2b48b693785
Cory B <cbipod33@gmail.com>
2020-02-27 11:17:58 -0500

    Update IWPUtilities.c
    5.02 TEST COMMIT

--- IWPUtilities.c
+++ IWPUtilities.c
@@ -69,7 +69,7 @@ EEProm_Write_Float(unsigned int ee_addr, void *obj_p);
 void noonMessage(void);
  **********************************/
 
-float codeRevisionNumber = 5.02;  //Current as of 2/27/2020
+float codeRevisionNumber = 5.02;  //Current as of 2/27/2020 //TEST COMMENT
 
 int __attribute__((space(eedata))) eeData; // Global variable located in EEPROM
 
